f88a4c34bf71c3b3402402176b4930bedf1c4044
PAWAN KUMAR SAHU <89339624+coderpawan@users.noreply.github.com>
2022-08-11 20:51:41 +0530

    First commit

---
+++ src/components/data.js
@@ -0,0 +1,56 @@
+export const sliderData =[
+
+     {
+        image:"https://images.unsplash.com/photo-1564507592333-c60657eea523?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+        heading:"TAJ MAHAL",
+        desc:"The Taj Mahal is an Islamic ivory-white marble mausoleum on the right bank of the river Yamuna in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor Shah Jahan  to house the tomb of his favourite wife, Mumtaz Mahal; it also houses the tomb of Shah Jahan himself. The tomb is the centrepiece of a 17-hectare complex, which includes a mosque and a guest house, and is set in formal gardens bounded on three sides by a crenellated wall."
+    },
+    {
+        image:"https://images.unsplash.com/photo-1566959621395-9200c65ba74b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+        heading:"UMAID BHAWAN PALACE",
+        desc:"Umaid Bhawan Palace, located in Jodhpur, Rajasthan, India, is one of the world's largest private residences. A part of the palace is managed by Taj Hotels. It is named after Maharaja Umaid Singh, grandfather of the present owner, Gaj Singh. The palace has 347 rooms and is the principal residence of the former Jodhpur royal family. A part of the palace is not a residence.The groundbreaking for the foundations of the building was carried out on 18 November 1929 by Maharaja Umaid Singh and the construction work was completed in 1943."
+    },
+    {
+        image:"https://images.unsplash.com/photo-1566873535350-a3f5d4a804b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        heading:"MEHRANGARH PALACE",
+        desc:"Mehrangarh Fort covers an area of 1,200 acres in Jodhpur, Rajasthan, India. The complex is located on a hilltop around 122 metres above the surrounding plain,[1] and was constructed circa 1459 by Rajput ruler Rao Jodha, though most of the existing structure is from the 17th century. Inside its boundaries there are several palaces known for their intricate carvings and expansive courtyards, as well as a museum housing various relics. A winding road leads to and from the city below. ."
+    },
+    {
+        image:"https://images.unsplash.com/photo-1496372412473-e8548ffd82bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1314&q=80",
+        heading:"VICTORIA MEMORIAL",
+        desc:"The Victoria Memorial is a large marble building in Central Kolkata, which was built between 1906 and 1921. It is dedicated to the memory of Queen Victoria, Empress of India from 1876 to 1901. It is now a museum under the auspices of the Ministry of Culture, and is the largest monument in the world which is dedicated to a royal.[2] The memorial lies on the Maidan and is one of the famous monuments of Kolkata."
+    },
+    
+    {
+        image:"https://images.unsplash.com/photo-1477587458883-47145ed94245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        heading:"HAWA MAHAL",
+        desc:"The Hawa Mahal is a palace in the city of Jaipur, India. Built from red and pink sandstone, it is on the edge of the City Palace, Jaipur, and extends to the Zenana, or women's chambers.The structure was built in 1799 by the Maharaja Sawai Pratap Singh, grandson of Maharaja Sawai Jai Singh, the founder of the city of Jaipur, India.[1] He was so inspired by the unique structure of Khetri Mahal that he built this grand and historical palace.It was designed by Lal Chand Ustad. "
+    },
+    {
+        image:"https://images.unsplash.com/photo-1517427677506-ade074eb1432?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+        heading:"GOLDEN TEMPLE",
+        desc:"The Golden Temple also known as the Harimandir Sahib) is a gurdwara located in the city of Amritsar, Punjab, India.[3][4] It is the preeminent spiritual site of Sikhism. It is one of the holiest sites in Sikhism, alongside the Gurdwara Darbar Sahib Kartarpur in Kartarpur, and Gurdwara Janam Asthan in Nankana Sahib.The man-made pool on the site of the temple was completed by the fourth Sikh Guru, Guru Ram Das, in 1577.In 1604, Guru Arjan placed a copy of the Adi Granth in Harmandir Sahib."
+    },
+   
+    {
+        image:"https://images.unsplash.com/photo-1523544261025-3159599b1fc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        heading:"MYSORE PALACE",
+        desc:"The Mysore Palace, also known as Amba Vilas Palace, is a historical palace and a royal residence (house). It is located in Mysore, Karnataka. It used to be the official residence of the Wadiyar dynasty and the seat of the Kingdom of Mysore. The palace is in the centre of Mysore, and faces the Chamundi Hills eastward. Mysore is commonly described as the 'City of Palaces', and there are seven palaces including this one. However, the Mysore Palace refers specifically to the one within the new fort."
+    },
+    {
+        image:"https://images.unsplash.com/photo-1597040663342-45b6af3d91a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+        heading:"HUMAYUN'S TOMB",
+        desc:"Humayun's tomb (Hindustani: Maqbara-i Humayun) is the tomb of the Mughal Emperor Humayun in Delhi, India. The tomb was commissioned by Humayun's chief consort, Empress Hamida Banu Begum, on patronage of her son Akbar, in 1558, and designed by Mirak Mirza Ghiyas and his son, Sayyid Muhammad,Persian architects chosen by her.It was the first garden-tomb on the Indian subcontinent,and is located in Nizamuddin East, Delhi, India, close to the Dina-panah Citadel, also known as Purana Qila (Old Fort), that Humayun found in 1533. "
+    },
+   
+   
+    {
+        image:"https://images.unsplash.com/photo-1587474260584-136574528ed5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        heading:"INDIA GATE",
+        desc:"The India Gate (formerly known as the All India War Memorial) is a war memorial located astride the Rajpath, on the eastern edge of the ceremonial axis of New Delhi, formerly called Kingsway. It stands as a memorial to 84,000 soldiers of the British Indian Army who died in between 1914 and 1921 in the First World War, in France, Flanders, Mesopotamia, Persia, East Africa, Gallipoli and elsewhere in the Near and the Far East, and the Third Anglo-Afghan War. 13,300 servicemen's names, including some soldiers and officers from the United Kingdom, are inscribed on the gate."
+    },
+ 
+ 
+  
+ 
+]
